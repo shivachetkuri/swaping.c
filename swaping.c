d09b3431f13b287d16f6ce3428c4c60a1360ff42
shivachetkuri <88610376+shivachetkuri@users.noreply.github.com>
2021-08-08 13:13:36 +0530

    created swaping.c
    This file prints swaping of numbers without using temporary variable in c on screen

---
+++ swaping.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b;
+	
+	printf("Enter the value of a: ");
+	scanf("%d",&a);
+	printf("Enter the value of b: ");
+	scanf("%d",&b);	
+	
+	//Numbers before swapping
+	printf("Before swapping... a: %d, b: %d\n",a,b);
+	
+	//swapping numbers
+	a = a+b;	
+	b = a-b;	
+  a = a-b; 
+	
+	//Numbers after swapping 
+	printf("After swapping...  a: %d, b: %d\n",a,b);
+
+	return 0;
+}
